ba536ab1ae411d9570357590870362546e48e23a
Ana Volpato <ana357volpato@gmail.com>
2021-06-05 23:59:16 -0300

    Fix save event picture

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -117,145 +117,151 @@ class Event extends Model
      */
     public function saveHistory($old_data, $request)
     {
-        $history = new EventHistory();
-        $history->club_code = getClubCode();
-        $history->effected_by = User::getAuthenticatedUserId();
-        $history->event_id = $this->id;
+        try {
 
-        // First register
-        if (! $old_data) {
-            $address = false;
-
-            // Address
-            if ($request->get('event_address_zip_code')) {
-                $address = $request->get('event_address_street_address');
-
-                if ($request->get('event_address_number')) {
-                    $address .= ', ' . $request->get('event_address_number');
+            $history = new EventHistory();
+            $history->club_code = getClubCode();
+            $history->effected_by = User::getAuthenticatedUserId();
+            $history->event_id = $this->id;
+    
+            // First register
+            if (! $old_data) {
+                $address = false;
+    
+                // Address
+                if ($request->get('event_address_zip_code')) {
+                    $address = $request->get('event_address_street_address');
+    
+                    if ($request->get('event_address_number')) {
+                        $address .= ', ' . $request->get('event_address_number');
+                    }
+    
+                    if ($request->get('event_address_complement')) {
+                        $address .= ', ' . $request->get('event_address_complement');
+                    }
+    
+                    $address .= " - " . $request->get('event_address_neighborhood');
+                    $address .= " - " . $request->get('event_address_city');
+                    $address .= " - " . $request->get('event_address_state');
                 }
-
-                if ($request->get('event_address_complement')) {
-                    $address .= ', ' . $request->get('event_address_complement');
+    
+                $history->resume = json_encode([
+                    'event' => array_merge($this->toArray(), [ 'address' => $address ]),
+                    'class' => array_key_exists('class', $request->all()) ? $request->all()['class'] : [],
+                ]);
+                
+    
+                $history->status = Event::DRAFT_STATUS;
+            } else {
+                $actual_data_event = $this->toArray();
+                $actual_class_data = $request->get('class');
+    
+                $diff_event = array();
+                $diff_class = array();
+    
+                $address = false;
+                $old_address = false;
+    
+                // Address
+                if ($request->get('event_address_zip_code')) {
+                    $address = $request->get('event_address_street_address');
+    
+                    if ($request->get('event_address_number')) {
+                        $address .= ', ' . $request->get('event_address_number');
+                    }
+    
+                    if ($request->get('event_address_complement')) {
+                        $address .= ', ' . $request->get('event_address_complement');
+                    }
+    
+                    $address .= " - " . $request->get('event_address_neighborhood');
+                    $address .= " - " . $request->get('event_address_city');
+                    $address .= " - " . $request->get('event_address_state');
                 }
-
-                $address .= " - " . $request->get('event_address_neighborhood');
-                $address .= " - " . $request->get('event_address_city');
-                $address .= " - " . $request->get('event_address_state');
-            }
-
-            $history->resume = json_encode([
-                'event' => array_merge($this->toArray(), [ 'address' => $address ]),
-                'class' => array_key_exists('class', $request->all()) ? $request->all()['class'] : [],
-            ]);
-            
-
-            $history->status = Event::DRAFT_STATUS;
-        } else {
-            $actual_data_event = $this->toArray();
-            $actual_class_data = $request->get('class');
-
-            $diff_event = array();
-            $diff_class = array();
-
-            $address = false;
-            $old_address = false;
-
-            // Address
-            if ($request->get('event_address_zip_code')) {
-                $address = $request->get('event_address_street_address');
-
-                if ($request->get('event_address_number')) {
-                    $address .= ', ' . $request->get('event_address_number');
+    
+                // Old Address
+                if ($old_data['event']['address']) {
+                    $old_address = $old_data['event']['address']['street_address'];
+    
+                    if ($old_data['event']['address']['number']) {
+                        $old_address .= ', ' . $old_data['event']['address']['number'];
+                    }
+    
+                    if ($old_data['event']['address']['complement']) {
+                        $old_address .= ', ' . $old_data['event']['address']['complement'];
+                    }
+    
+                    $old_address .= " - " . $old_data['event']['address']['neighborhood'];
+                    $old_address .= " - " . $old_data['event']['address']['city'];
+                    $old_address .= " - " . $old_data['event']['address']['state'];
                 }
-
-                if ($request->get('event_address_complement')) {
-                    $address .= ', ' . $request->get('event_address_complement');
-                }
-
-                $address .= " - " . $request->get('event_address_neighborhood');
-                $address .= " - " . $request->get('event_address_city');
-                $address .= " - " . $request->get('event_address_state');
-            }
-
-            // Old Address
-            if ($old_data['event']['address']) {
-                $old_address = $old_data['event']['address']['street_address'];
-
-                if ($old_data['event']['address']['number']) {
-                    $old_address .= ', ' . $old_data['event']['address']['number'];
-                }
-
-                if ($old_data['event']['address']['complement']) {
-                    $old_address .= ', ' . $old_data['event']['address']['complement'];
-                }
-
-                $old_address .= " - " . $old_data['event']['address']['neighborhood'];
-                $old_address .= " - " . $old_data['event']['address']['city'];
-                $old_address .= " - " . $old_data['event']['address']['state'];
-            }
-
-            // Clean event date
-            foreach(['date', 'date_limit'] as $date_field) {
-                if (array_key_exists($date_field, $old_data['event'])) {
-                    $old_data['event'][$date_field] = substr($old_data['event'][$date_field], 0, 10);
-                }
-            }
-            // Clean class date
-            if (array_key_exists('class', $old_data)) {
-                foreach($old_data['class'] as $i_old_class => $v_old_class) {
-                    if (array_key_exists('start_subscription_date', $v_old_class)) {
-                        $old_data['class'][$i_old_class]['start_subscription_date']  = substr($v_old_class['start_subscription_date'], 0, 10);
+    
+                // Clean event date
+                foreach(['date', 'date_limit'] as $date_field) {
+                    if (array_key_exists($date_field, $old_data['event'])) {
+                        $old_data['event'][$date_field] = substr($old_data['event'][$date_field], 0, 10);
                     }
                 }
-            }
-     
-            foreach($actual_class_data as $i_actual_class_data => $v_actual_class_data) {
-                // $actual_class_data[$i_actual_class_data] = $v_actual_class_data->toArray();
-
-                // $actual_class_data[$i_actual_class_data] = [
-                //     // 'event_id' => $actual_class_data[$i_actual_class_data]['event_id'],
-                //     // 'member_class_id' => $actual_class_data[$i_actual_class_data]['member_class_id'],
-                //     'start_subscription_date' => $actual_class_data[$i_actual_class_data]['start_subscription_date'],
-                //     'vehicle_value' => $actual_class_data[$i_actual_class_data]['vehicle_value'],
-                //     'participant_value' => $actual_class_data[$i_actual_class_data]['participant_value'],
-                //     'companion_value' => $actual_class_data[$i_actual_class_data]['companion_value'],
-                //     // 'created_at' => $actual_class_data[$i_actual_class_data]['created_at'],
-                //     // 'updated_at' => $actual_class_data[$i_actual_class_data]['updated_at'],
-                // ];
-
-
-                if (array_key_exists($i_actual_class_data, $old_data['class'])) {
-                    $diff_class[$i_actual_class_data] = array_diff($actual_class_data[$i_actual_class_data], $old_data['class'][$i_actual_class_data]);
+                // Clean class date
+                if (array_key_exists('class', $old_data)) {
+                    foreach($old_data['class'] as $i_old_class => $v_old_class) {
+                        if (array_key_exists('start_subscription_date', $v_old_class)) {
+                            $old_data['class'][$i_old_class]['start_subscription_date']  = substr($v_old_class['start_subscription_date'], 0, 10);
+                        }
+                    }
                 }
-            }
-            
-            unset($actual_data_event['class_data']);
-
-            if ($address != $old_address) {
-                $diff_event = array_diff(array_merge($actual_data_event, [ 'address' => $address ]), array_merge($old_data['event'], [ 'address' => $old_address]));
-            } else {
-                if (isset($actual_data_event['address'])) {
-                    unset($actual_data_event['address']);
+    
+                foreach($actual_class_data as $i_actual_class_data => $v_actual_class_data) {
+                    // $actual_class_data[$i_actual_class_data] = $v_actual_class_data->toArray();
+    
+                    // $actual_class_data[$i_actual_class_data] = [
+                    //     // 'event_id' => $actual_class_data[$i_actual_class_data]['event_id'],
+                    //     // 'member_class_id' => $actual_class_data[$i_actual_class_data]['member_class_id'],
+                    //     'start_subscription_date' => $actual_class_data[$i_actual_class_data]['start_subscription_date'],
+                    //     'vehicle_value' => $actual_class_data[$i_actual_class_data]['vehicle_value'],
+                    //     'participant_value' => $actual_class_data[$i_actual_class_data]['participant_value'],
+                    //     'companion_value' => $actual_class_data[$i_actual_class_data]['companion_value'],
+                    //     // 'created_at' => $actual_class_data[$i_actual_class_data]['created_at'],
+                    //     // 'updated_at' => $actual_class_data[$i_actual_class_data]['updated_at'],
+                    // ];
+    
+    
+                    if (array_key_exists($i_actual_class_data, $old_data['class'])) {
+                        $diff_class[$i_actual_class_data] = array_diff($actual_class_data[$i_actual_class_data], $old_data['class'][$i_actual_class_data]);
+                    }
                 }
-
-                if (isset($old_data['event']['address'])) {
-                    unset($old_data['event']['address']);
+                
+                unset($actual_data_event['class_data']);
+    
+                if ($address != $old_address) {
+                    $diff_event = array_diff(array_merge($actual_data_event, [ 'address' => $address ]), array_merge($old_data['event'], [ 'address' => $old_address]));
+                } else {
+                    if (isset($actual_data_event['address'])) {
+                        unset($actual_data_event['address']);
+                    }
+    
+                    if (isset($old_data['event']['address'])) {
+                        unset($old_data['event']['address']);
+                    }
+    
+                    $diff_event = array_diff($actual_data_event, $old_data['event']);
                 }
-
-                $diff_event = array_diff($actual_data_event, $old_data['event']);
+    
+                $history->resume = json_encode([
+                    'event' => $diff_event,
+                    'old_event' => $old_data['event'],
+                    'class' => $diff_class,
+                    'old_class' => array_key_exists('class', $old_data) ? $old_data['class'] : [],
+                ]);
+    
+                $history->status = $this->status;
             }
-
-            $history->resume = json_encode([
-                'event' => $diff_event,
-                'old_event' => $old_data['event'],
-                'class' => $diff_class,
-                'old_class' => array_key_exists('class', $old_data) ? $old_data['class'] : [],
-            ]);
-
-            $history->status = $this->status;
+    
+            $history->save();
+        } catch (\Exception $e) 
+        {
+            \Log::error("Erro ao salvar histórico: " . $e->getMessage());
         }
-
-        $history->save();
     }
 
     public function saveClassData($classes)
