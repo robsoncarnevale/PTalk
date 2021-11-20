50a71fa7b4c53951e97fc41483039472314b2e6e
Daniel Bruno <danielbrunogbs@gmail.com>
2021-11-20 18:57:38 -0300

    #10 - Implementado listagem de contas bancárias

--- app/Http/Controllers/BankAccountController.php
+++ app/Http/Controllers/BankAccountController.php
@@ -16,24 +16,18 @@ class BankAccountController extends Controller
 
     public function index()
     {
-        $accounts = BankAccount::select(
-                                    'id',
-                                    DB::raw('SUM(balance) AS `balance_resume`'),
-                                    'uuid',
-                                    'account_number',
-                                    'balance',
-                                    'bank_account_type_id',
-                                    'status_id',
-                                    'created_at',
-                                    'updated_at'
-                                )
-                                ->orderBy('id', 'desc')
+        $accounts = BankAccount::orderBy('id', 'desc')
                                 ->orderBy('bank_account_type_id', 'asc')
                                 ->jsonPaginate(20);
 
-        // dd($accounts);
+        $resume = BankAccount::sum('balance');
 
         return [
+            'status' => 'success',
+            'resume' => [
+                'total_balances' => $resume,
+                'total_accounts' => count($accounts['data'])
+            ],
             'data' => BankAccountResource::collection($accounts['data']),
             'paginator' => $accounts['paginator']
         ];
