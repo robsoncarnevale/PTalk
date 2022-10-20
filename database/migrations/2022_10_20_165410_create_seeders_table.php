99e8174b4c29cfce7f572fa50c2c99a6760196b6
Lucas Daniel Beltrame <lucasdanielbeltrame@hotmail.com>
2022-10-20 18:11:14 -0300

    change name class

--- database/migrations/2022_10_20_165410_create_seeders_table.php
+++ database/migrations/2022_10_20_165410_create_seeders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSedersTable extends Migration
+class CreateSeedersTable extends Migration
 {
     /**
      * Run the migrations.
